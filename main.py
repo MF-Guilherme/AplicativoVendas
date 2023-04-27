beb12b47ae28366507d04bdb3daa23b7130af527
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-04-27 17:53:31 -0300

    Botão e função para mudar de tela, criados

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ class HomePage(Screen):
 class AjustesPage(Screen):
     pass
 
+
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
 
     def build(self):
         return GUI
-    
 
+    def mudar_tela(self, id_tela):
+        print(id_tela)
+        gerenciador_telas = self.root.ids["screen_manager"] #self.root é o main.kv, a minha página de screen manager
+        gerenciador_telas.current = id_tela #qual tela atual (current) vc está falando? da que chegar no parametro
 MainApp().run()
