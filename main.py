b1668db78c21f0592a118b74b068f05a791c94cf
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-05-02 11:39:17 -0300

    Organizando os arquivos e criando classes para botoes em imagens e labels

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-
-
-class HomePage(Screen):
-    pass
-
-
-class AjustesPage(Screen):
-    pass
-
+from telas import *
+from botoes import *
 
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
@@ -21,4 +13,5 @@ class MainApp(App):
         print(id_tela)
         gerenciador_telas = self.root.ids["screen_manager"] #self.root é o main.kv, a minha página de screen manager
         gerenciador_telas.current = id_tela #qual tela atual (current) vc está falando? da que chegar no parametro
+
 MainApp().run()
