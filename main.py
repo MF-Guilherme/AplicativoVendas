e41dba8afb88ab4f51473129d3ea4472ce359e69
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-05-02 23:25:35 -0300

    Criada a função on_start para acessar o banco de dados firebase

--- main.py
+++ main.py
@@ -2,16 +2,27 @@ from kivy.app import App
 from kivy.lang import Builder
 from telas import *
 from botoes import *
+import requests
 
 GUI = Builder.load_file("main.kv")
-class MainApp(App):
 
+
+class MainApp(App):
+    id_usuario = 1
+    
     def build(self):
         return GUI
-
+    
+    def on_start(self):
+        requisicao = requests.get(f"https://aplicativovendashash-4e118-default-rtdb.firebaseio.com/{self.id_usuario}.json")
+        requisicao_dic = requisicao.json()
+        avatar = requisicao_dic['avatar']
+        print(avatar)
+        
     def mudar_tela(self, id_tela):
         print(id_tela)
-        gerenciador_telas = self.root.ids["screen_manager"] #self.root é o main.kv, a minha página de screen manager
-        gerenciador_telas.current = id_tela #qual tela atual (current) vc está falando? da que chegar no parametro
+        gerenciador_telas = self.root.ids["screen_manager"]  # self.root é o main.kv, a minha página de screen manager
+        gerenciador_telas.current = id_tela  # qual tela atual (current) vc está falando? da que chegar no parametro
+
 
 MainApp().run()
